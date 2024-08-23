c986ac4ab482201787785fca64821129eb2b8c3d
Rodrigo-X33 <rodrigo.chaconx33@gmail.com>
2024-08-22 20:34:12 -0600

    ajustes de la página del home

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,29 @@
 import DeployButton from "../components/DeployButton";
-import Header from "@/components/Header";
-import { redirect } from "next/navigation";
+import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-
+import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
+import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
+import Header from "@/components/Header";
 
 export default async function Index() {
-  const supabase = createClient();
+  const canInitSupabaseClient = () => {
 
-  const { data: { user }} = await supabase.auth.getUser();
+    try {
+      createClient();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
 
-  if (!user) {
-    return redirect("/login");
-  }
+  const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
+          {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
 
@@ -25,6 +31,7 @@ export default async function Index() {
         <Header />
         <main className="flex-1 flex flex-col gap-6 px-4">
           <h2 className="font-bold text-2xl mb-4">Next steps</h2>
+          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
         </main>
       </div>
 
